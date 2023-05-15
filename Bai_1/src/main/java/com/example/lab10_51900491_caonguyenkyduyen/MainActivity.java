9e11e80a6cfe598f25391f9414a096a6abbda646
Cao Nguyễn Kỳ Duyên <51900491@student.tdtu.edu.vn>
2023-05-15 17:49:11 +0700

    //51900491

--- Bai_1/src/main/java/com/example/lab10_51900491_caonguyenkyduyen/MainActivity.java
+++ Bai_1/src/main/java/com/example/lab10_51900491_caonguyenkyduyen/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.lab10_51900491_caonguyenkyduyen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.myMap);
+        mapFragment.getMapAsync(MainActivity.this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.map = googleMap;
+        LatLng vietnam = new LatLng(14.0583, 108.2772); // 14.0583° N, 108.2772° E
+        this.map.addMarker(new MarkerOptions().position(vietnam).title("Marker in Vietnam"));
+        this.map.moveCamera(CameraUpdateFactory.newLatLng(vietnam));
+
+        this.map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(latLng.latitude + " : "+ latLng.longitude);
+                // Clear previously click position.
+                map.clear();
+                // Zoom the Marker
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                // Add Marker on Map
+                map.addMarker(markerOptions);
+            }
+        });
     }
 }
---
+++ Bai_1/src/main/java/com/example/lab10_51900491_caonguyenkyduyen/MapActivity.java
@@ -0,0 +1,35 @@
+package com.example.lab10_51900491_caonguyenkyduyen;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+   // private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
